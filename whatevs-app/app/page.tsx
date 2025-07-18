039034da00776de8cfd1b910f80ae04a1fa72d43
LohYenKuan <lohyenkuan@gmail.com>
2025-07-18 21:44:21 +0800

    fix tailwind issue

--- whatevs-app/app/page.tsx
+++ whatevs-app/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
 import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
     <main>
-      <Dashboard/>
+      <Dashboard />
     </main>
   );
 }
- 
+
